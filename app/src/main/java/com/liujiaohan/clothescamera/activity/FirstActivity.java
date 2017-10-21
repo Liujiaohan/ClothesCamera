217d78d8ae5bd5d1030fcd9f4e8de7775ec7d467
enble du <384509085@qq.com>
2017-10-22 01:12:55 +0800

    firstpage

---
+++ app/src/main/java/com/liujiaohan/clothescamera/activity/FirstActivity.java
@@ -0,0 +1,35 @@
+package com.liujiaohan.clothescamera.activity;
+
+import android.graphics.Color;
+import android.os.Build;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.liujiaohan.clothescamera.R;
+
+public class FirstActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first);
+
+        if (Build.VERSION.SDK_INT >= 21) {
+            View decorView = getWindow().getDecorView();
+            int option = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
+            decorView.setSystemUiVisibility(option);
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+
+            Button btn = (Button) findViewById(R.id.firstpage_btn);
+            btn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                }
+            });
+        }
+    }
+}
